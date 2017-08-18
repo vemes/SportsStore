e3a55b1a9c879537fcdd734d8907131a9b2f8b12
Marek Chrobok <Marek Chrobok>
2017-08-18 21:03:23 +0200

    Add project files.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System.Linq;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(int page = 1)
+        => View(new ProductsListViewModel
+        {
+            Products = repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        });
+    }
+}
