cc5559d573b5e7405b8a6883d7ab9e743d01b89c
Marek Chrobok <marek.chrobok@outlook.com>
2017-08-18 23:04:30 +0200

    Added category support.

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -14,10 +14,11 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         => View(new ProductsListViewModel
         {
             Products = repository.Products
+            .Where(p => category == null || p.Category == category)
             .OrderBy(p => p.ProductID)
             .Skip((page - 1) * PageSize)
             .Take(PageSize),
@@ -26,7 +27,8 @@ namespace SportsStore.Controllers
                 CurrentPage = page,
                 ItemsPerPage = PageSize,
                 TotalItems = repository.Products.Count()
-            }
+            },
+            CurrentCategory = category
         });
     }
 }
